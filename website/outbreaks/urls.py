261ce0431d1c699a3104cd81cf7e2e40a9e5e868
Alejandro Lopez <alejandrolopez1592@gmail.com>
2026-04-25 15:34:55 -0600

    UpdatedDataAndVisualizationsAndRiskFactorLogic

--- website/outbreaks/urls.py
+++ website/outbreaks/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("api/summary/", views.api_summary, name="api_summary"),
     path("api/countries/", views.api_countries, name="api_countries"),
     path("api/timeseries/", views.api_timeseries, name="api_timeseries"),
+    path("api/monthly-risk/", views.api_monthly_risk, name="api_monthly_risk"),
     path("api/hotspots/", views.api_hotspots, name="api_hotspots"),
 ]
